fe06c55aa6df7c5e59723e577d6ad58f677d1df8
Efraim Andrade <efraim.dev@gmail.com>
2023-02-06 23:43:09 -0300

    added the rest of base tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
 export * from './color'
+
+export * from "./radii";
+export * from "./space";
+
+export * from "./fonts";
+export * from "./font-sizes";
+export * from "./font-weights";
+export * from "./line-heights";
