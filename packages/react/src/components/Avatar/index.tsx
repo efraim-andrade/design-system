5e00d93f8452ba3fed50f69e6dcc2c4444e603d5
Efraim Andrade <efraim.dev@gmail.com>
2023-02-08 16:24:40 -0300

    added avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from 'react'
+import { User } from 'phosphor-react'
+
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar({ src }: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage src={src} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
