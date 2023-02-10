a0c3f6ad211d392ef19b4f6792f3bf7bdad6eaa4
Efraim Andrade <efraim.dev@gmail.com>
2023-02-09 22:34:53 -0300

    added the import helpers on eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ["@rocketseat/eslint-config/react"],
+  plugins: ["import-helpers"],
+  rules: {
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        "newlinesBetween": "always",
+        "groups": [["/^react/", "module"], "/^~/", ["parent", "sibling", "index"]],
+        "alphabetize": { "order": "ignore", "ignoreCase": true }
+      }
+    ]
+  }
 };
